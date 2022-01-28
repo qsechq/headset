76c1f16b9699924523482cc6bbd53caedc4c448a
Nikita <ss16nik@gmail.com>
2022-01-28 10:54:50 +0400

    fix burger

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// простой способ
 let hamburger  = document.querySelector('.hamburger ');
 let overlay = document.querySelector('.overlay');
+let links = document.querySelectorAll('.nav__link');
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
 
 function toggleMenu(){
   hamburger.classList.toggle('hamburger--active');
